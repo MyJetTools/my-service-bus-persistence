99ac64e69b07841510c20acbaf0450dbed5defde
amigin <amigin@gmail.com>
2022-05-21 12:23:52 +0200

    Fixed bug with calculating duration

--- src/utils/duration_utils.rs
+++ src/utils/duration_utils.rs
@@ -10,7 +10,7 @@ pub fn duration_to_string(d: Duration) -> String {
 }
 
 fn format_duration(mut secs: u64) -> String {
-    let days = secs / 3600 * 24;
+    let days = secs / (3600 * 24);
 
     secs = secs - days * 3600 * 24;
 
@@ -23,7 +23,7 @@ fn format_duration(mut secs: u64) -> String {
     secs = secs - mins * 60;
 
     if days > 0 {
-        return format!("{}:{:02}:{:02}:{:02}", days, hours, mins, secs);
+        return format!("{}d:{:02}:{:02}:{:02}", days, hours, mins, secs);
     } else {
         return format!("{:02}:{:02}:{:02}", hours, mins, secs);
     }
@@ -35,8 +35,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_format_duration() {
+    fn test_minutes() {
         assert_eq!("00:01:00", format_duration(60));
         assert_eq!("00:01:01", format_duration(61));
+
+        assert_eq!("00:02:00", format_duration(60 * 2));
+        assert_eq!("00:02:01", format_duration(60 * 2 + 1));
+
+        assert_eq!("00:59:00", format_duration(60 * 59));
+        assert_eq!("00:59:59", format_duration(60 * 59 + 59));
+    }
+
+    #[test]
+    fn test_hours() {
+        assert_eq!("01:00:00", format_duration(60 * 60));
+        assert_eq!("01:01:00", format_duration(60 * 60 + 60));
+        assert_eq!("01:01:01", format_duration(60 * 60 + 61));
+    }
+
+    #[test]
+    fn test_days() {
+        assert_eq!("1d:00:00:00", format_duration(60 * 60 * 24));
+        assert_eq!("1d:00:00:01", format_duration(60 * 60 * 24 + 1));
     }
 }
