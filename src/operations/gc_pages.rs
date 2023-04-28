99939c6d52b420ee8d33e5dd8d34876b272cda59
amigin <amigin@gmail.com>
2023-04-28 15:51:01 +0300

    Now we GC one topic until everything go to archive

--- src/operations/gc_pages.rs
+++ src/operations/gc_pages.rs
@@ -5,7 +5,7 @@ use crate::{app::AppContext, topic_data::TopicData};
 use super::OperationError;
 
 pub async fn gc_pages(app: &AppContext, topic_data: Arc<TopicData>) -> Result<(), OperationError> {
-    if let Some(page_to_gc) = topic_data.pages_list.gc().await {
+    while let Some(page_to_gc) = topic_data.pages_list.gc().await {
         crate::operations::archive_io::save_sub_page(app, &topic_data, &page_to_gc).await;
     }
 
