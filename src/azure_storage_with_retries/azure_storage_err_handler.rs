fb02954a27fe953f4f5e9f2f353360821affa29b
amigin <amigin@gmail.com>
2023-04-28 12:05:22 +0300

    Fixes

--- src/azure_storage_with_retries/azure_storage_err_handler.rs
+++ src/azure_storage_with_retries/azure_storage_err_handler.rs
@@ -7,23 +7,18 @@ pub async fn handle_error_and_create_blob(
 ) -> Result<AzureStorageError, AzureStorageError> {
     match err {
         AzureStorageError::ContainerNotFound => {
-            println!("Creating container");
             page_blob.create_container_if_not_exists().await?;
-            println!("Creating Blob");
             page_blob
                 .create_blob_if_not_exists(init_page_blob_size)
                 .await?;
 
-            println!("Created container and blob");
             Ok(err)
         }
         AzureStorageError::BlobNotFound => {
-            println!("Creating blob");
             page_blob
                 .create_blob_if_not_exists(init_page_blob_size)
                 .await?;
 
-            println!("Created blob");
             Ok(err)
         }
         _ => Err(err),
